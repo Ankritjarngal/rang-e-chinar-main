6c414439c7afed52a7bbd5635713ca5f6cd917f7
ikjotkour27 <ikjotkourmultani.27@gmail.com>
2025-04-06 18:26:44 +0530

    m

--- src/pages/Events/components/PerformingEventsDetailsLayout.jsx
+++ src/pages/Events/components/PerformingEventsDetailsLayout.jsx
@@ -62,7 +62,7 @@ export default function PerformingEventsDetailsLayout({data, index, breadcrumbs}
         <div className="flex gap-4">
           <button className="px-6 md:px-8 py-2 ShadowBlur text-sm md:text-base rounded-md font-semibold text-gray-800 bg-primary border border-primary focus:outline-none text-center mb-4">
             <Link
-              to={"/register"}
+              to={"/schedule"}
             >
               Register
             </Link>
