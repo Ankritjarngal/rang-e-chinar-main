5f9765d7cea17467fd97cc45a629decceb775412
ankrit <jarngalankrit@gmail.com>
2025-04-08 16:55:59 +0530

    navbar on register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,19 +1,20 @@
+import Header from "../../Components/Header";
 export default function Register() {
   return (
     <>
-      {/* <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<RegisterHome />} />
-        <Route path="nit-register" element={<EventRegister />} />
-      </Routes> */}
+      {/* New transparent black div without dimensions */}
+      <div
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', backdropFilter: 'blur(5px)' }}
+      >
+        <Header></Header>
+      </div>
 
-      {/* Coming Soon Page */}
+      {/* Original Coming Soon Section */}
       <div
         className="flex flex-col items-center justify-center min-h-screen bg-[url('/public/common/bg.jpg')] bg-cover bg-center text-white"
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', backdropFilter: 'blur(5px)' }}
       >
         <h1 className="text-3xl md:text-6xl font-bold mb-4">Coming Soon</h1>
-
       </div>
     </>
   );
