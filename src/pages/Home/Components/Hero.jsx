55a502a72704eb296d18a3fef7e3df9ed9c0126f
ankrit <jarngalankrit@gmail.com>
2025-04-10 15:21:49 +0530

    registration link added

--- src/pages/Home/Components/Hero.jsx
+++ src/pages/Home/Components/Hero.jsx
@@ -175,8 +175,9 @@ function Hero() {
               data-aos="fade-up" 
               data-aos-delay="500"
               className="px-8 py-4 rounded-full text-lg md:text-xl font-medium bg-gradient-to-r from-green-600 to-green-500 hover:from-green-500 hover:to-green-400 border border-green-300/30 hover:border-green-300/60 hover:shadow-lg hover:shadow-green-500/30 transform hover:scale-105 transition-all duration-300 font-sans tracking-wider"
+              onClick={() => window.open("https://rangechinar-registration-from.vercel.app", "_blank")}
             >
-              REGISTRATIONS COMING SOON
+              REGISTER NOW
             </button>
           </div>
         </div>
