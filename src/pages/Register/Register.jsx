102818d8682cc07b0b91c0490e93abed007e7961
ankrit <jarngalankrit@gmail.com>
2025-10-19 23:25:55 +0530

    changes

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,14 @@
 import Header from "../../Components/Header";
+import { useEffect } from "react";
 
 export default function Register() {
+  useEffect(() => {
+    window.location.href = "https://rangechinar-registration-from.vercel.app/";
+
+ 
+  }, []);
   return (
     <>
-      <a href="https://rangechinar-registration-from.vercel.app/"></a>
     </>
   );
 }
